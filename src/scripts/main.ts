b2f9f099d32c63ca1a6bec2d0c381b15b4a9e22f
Antony1060 <antoniostignjedec@gmail.com>
2022-05-25 21:50:07 +0200

    Move week count to computed and enable days until end

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,6 +6,7 @@ const weekCount: HTMLHeadingElement = document.querySelector(".week");
 // first monday
 // -2 to account for weekends, because we start displaying correct timetable at weekend before that week
 const START_TIME = new Date(2021, 8, 6 - 2).getTime();
+const END_TIME = new Date(2022, 5, 21).getTime();
 // will change this as years go on
 const START_WEEK = second;
 START_WEEK != first && swapElements(first, second);
@@ -18,8 +19,17 @@ isFirstWeek || swapElements(first, second);
 if([0, 1].includes(daysSince % 7)) first.getElementsByTagName("h2")[0].innerText += " (iduci tjedan)"
 else first.getElementsByTagName("h2")[0].innerText += " (trenutno)"
 
-const displayLeft = false;
-weekCount.innerText += ` ${Math.ceil((daysSince - 1) / 7)}/42${displayLeft ? ` (${Math.floor((new Date(2022, 5, 21).getTime() - Date.now()) / (1000 * 60 * 60 * 24))} dan(a) do kraja)` : ''}`;
+const daysUntilEnd = Math.floor((END_TIME - Date.now()) / (1000 * 60 * 60 * 24));
+
+const weeksSince = Math.ceil((daysSince - 1) / 7)
+const totalWeeks = Math.ceil((END_TIME - START_TIME) / (1000 * 60 * 60 * 24 * 7));
+
+const displayLeft = true;
+
+if (daysUntilEnd === 0)
+     weekCount.innerHTML += `Gotovo!`
+else
+    weekCount.innerText = `Tjedan ${weeksSince}/${totalWeeks}${displayLeft ? ` (${daysUntilEnd} dan${daysUntilEnd.toString().endsWith("1") ? "" : "a"} do kraja)` : ''}`;
 
 if(daysSince % 7 >= 2) {
     let idx = daysSince % 7 - 1; // Pon -> 1; Uto -> 2; etc.
